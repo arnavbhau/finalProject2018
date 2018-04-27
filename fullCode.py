ff721f15a68e434762c443710de767ded75f6967
arnavbhau <38510355+arnavbhau@users.noreply.github.com>
2018-04-27 10:19:13 -0700

    Add files via upload

--- fullCode.py
+++ fullCode.py
@@ -5,12 +5,12 @@
 # i got the background image from http://www.mariouniverse.com/maps/ds/nsmb
 
 # importing libraries
-import random, math, time, sys
-import pygame
+import random, math, time, sys, pygame
 from pygame.locals import *
 from player import Player
 from settings import Settings
 from pygame.sprite import Sprite
+import gameFunctions as gf
 
 # the run game function 
 def setup():
@@ -19,37 +19,33 @@ def setup():
     marioSettings = Settings()
     global screen
     screen = pygame.display.set_mode((marioSettings.screenWidth, marioSettings.screenHeight))
-    #initialize
+    #initialize pygame with the screen
     pygame.init()
     pygame.display.set_caption("Super Mario Bros!")
 
-# making the character group and the background
+# making the character and the background
 background = pygame.image.load("finalProject/assets/images/marioplain.png")
 # allSpritesList = pygame.sprite.Group()
 
 # defining some variables for the program running
 width = 600
-x = 0
+keyvar = 0
 while True:
     setup()
-
+    
     # makes the screen move continuously via blitting
     background = background.convert()
-    relx = x % background.get_rect().width
+    relx = keyvar % background.get_rect().width
     screen.blit(background, [relx - background.get_rect().width, 0])
     if relx < width:
         screen.blit(background, [relx, 0])
-    x -= 1
+    keyvar -= 1
 
     # loads the characters on the screen
-    mario = Player(screen)
+    mario = Player(screen, 3, 50)
+    mario.setLocation(300, 0)
+    mario.do()
     mario.blitme()
-
     # updates the screen
     pygame.display.flip()
 
-    # to quit the game if the user clicks on the x button
-    for event in pygame.event.get():
-        if event.type == QUIT or (event.type == KEYDOWN and event.key == K_ESCAPE):
-            pygame.quit()
-            sys.exit()
